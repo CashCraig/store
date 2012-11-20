ecc3e3c01a9e37ec59a11f25c1703ea18b70bfd3
Cash Craig <ccraig35@academic.rrc.ca>
2012-11-20 15:03:47 -0600

    Created repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'f2fef37e0f8d843a5c90582d77baec43aa726757fd52aa6c21780a96de260c74a1b664384dd5ee64dea422942f9e04facaef16e685876abeab1b86d728a2825f'
