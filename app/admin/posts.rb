32c4c901a43d44ec8ac760c024061826d2a1e5e4
Cash Craig <ccraig35@academic.rrc.ca>
2012-11-26 08:59:49 -0600

    Modified ActiveAdmin Resources
    - Customization of Product/Post resources; can now view images

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Post, :as => "News" do
-  
+    index do
+		column :image do |product|
+			image_tag(product.image, :width => '50px')
+		end	
+    column :content
+    column :created_at
+		column :updated_at
+
+    default_actions
+  end
 end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,20 @@
-ActiveAdmin.register Product do
-  
+ActiveAdmin.register Product do 
+  index do
+		column :image do |product|
+			image_tag(product.image, :width => '100px')
+		end	
+    column :name
+    column :description do |product|
+			truncate(product.description, :length => 150)
+		end
+		column :price
+		column :sale_price
+		column :stock_quantity
+		column :order_quantity
+		column :status
+    column :created_at
+		column :updated_at
+
+    default_actions
+  end
 end
